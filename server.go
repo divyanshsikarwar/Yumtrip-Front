2b8b492096c46eefdf3342ca8cd709377a2462d9
Divyansh Sikarwar <54525621+divyanshsikarwar@users.noreply.github.com>
2024-04-13 17:34:38 +0530

    Added RabbitMQ, wrote consumer, wrote all the crons

---
+++ server.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	apiv1 "yumtrip/api/v1"
+	cronworkers "yumtrip/workers/cronWorkers"
+	rabbitconsumers "yumtrip/workers/rabbitConsumers"
+
+	"github.com/gorilla/mux"
+)
+
+func main(){
+	router := mux.NewRouter()
+	v1Router := router.PathPrefix("/api/v1").Subrouter()
+	v1Router.Handle("/", apiv1.Router())
+
+	log.Println("Server is starting...")
+
+	cronworkers.Init()
+	rabbitconsumers.Init()
+
+	// Start the server
+	err := http.ListenAndServe(":8080", router)
+	if err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
+}
